7da4a098a78044690c7121cc131780faf55f536a
Popov85 <andrey.popov85@gmail.com>
2016-06-08 12:54:33 +0300

    Binary tree forming is added

--- src/com/goit/g2popov/module05/task02/SortArray.java
+++ src/com/goit/g2popov/module05/task02/SortArray.java
@@ -24,4 +24,63 @@ public class SortArray {
                 }
                 return this.anArray;
         }
+
+        public void formBinaryTree() {
+                TreeNode rootNode = initialiseRoot(0, anArray[0]);
+                int arrayLength = this.anArray.length;
+                for (int i = 1; i<arrayLength; i++) {
+                        formSubTree(i, anArray[i], rootNode);
+                }
+                // Launch a method to traverse the tree built (rootNode);
+        }
+
+        private TreeNode initialiseRoot(int id, int value) {
+                TreeNode rootNode = new TreeNode(id, value);
+                return rootNode;
+        }
+
+        private TreeNode createNode(int id, int value, TreeNode node) {
+                TreeNode rootNode = new TreeNode(id, value, node);
+                return rootNode;
+        }
+
+        private void formSubTree(int arrayId, int arrayValue, TreeNode root) {
+                if (arrayValue < root.value) {
+                        if (root.left == null) {
+                                TreeNode leftNode = createNode(arrayId, arrayValue, root);
+                                root.left = leftNode;
+                        }
+                        else {
+                                root = root.left;
+                                formSubTree(arrayId, arrayValue, root);
+                        }
+                }
+                else {
+                        if (root.right == null) {
+                                TreeNode rightNode = createNode(arrayId, arrayValue, root);
+                                root.right = rightNode;
+                        }
+                        else {
+                                root = root.right;
+                                formSubTree(arrayId, arrayValue, root);
+                        }
+                }
+        }
+}
+
+class TreeNode {
+        int id;
+        int value;
+        TreeNode parent;
+        TreeNode left;
+        TreeNode right;
+        public TreeNode(int id, int value) {
+                this.id = id;
+                this.value = value;
+        }
+        public TreeNode(int id, int value, TreeNode parent) {
+                this.id = id;
+                this.value = value;
+                this.parent = parent;
+        }
 }
